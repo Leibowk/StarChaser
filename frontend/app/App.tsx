16cc4c8ca2c81f212547b8216ea2d86a968ca2be
Leibowk <kyle.leibowitz@gmail.com>
2025-11-12 15:56:08 -0800

    Bit more traditional map

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -19,11 +19,36 @@ const MAPLIBRE_HTML = `
     <script>
         const map = new maplibregl.Map({
             container: 'map',
-            style: 'https://demotiles.maplibre.org/style.json',
-            center: [-98.5795, 39.8283],
-            zoom: 4
+            style: {
+                version: 8,
+                sources: {
+                    'raster-tiles': {
+                        type: 'raster',
+                        tiles: [
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        ],
+                        tileSize: 256,
+                        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    }
+                },
+                layers: [
+                    {
+                        id: 'simple-tiles',
+                        type: 'raster',
+                        source: 'raster-tiles',
+                        minzoom: 0,
+                        maxzoom: 22
+                    }
+                ]
+            },
+            center: [-98.5795, 39.8283], // Center of USA
+            zoom: 3
+            // center: [-120.77, 47.20], // Center of WA
+            // zoom: 5
         });
+        
         map.addControl(new maplibregl.NavigationControl());
+        map.addControl(new maplibregl.ScaleControl(), 'bottom-left');
     </script>
 </body>
 </html>
@@ -37,6 +62,7 @@ export default function App() {
         style={styles.webview}
         javaScriptEnabled={true}
         domStorageEnabled={true}
+        allowsInlineMediaPlayback={true}
       />
       <StatusBar style="auto" />
     </View>
